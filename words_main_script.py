a876e32c0528d818e08002cc0fa99107d41c19c0
dodgejoel <dodgejoel@gmail.com>
2014-04-07 20:32:11 -0400

    output format changed

--- words_main_script.py
+++ words_main_script.py
@@ -3,6 +3,9 @@ results.'''
 
 import play_finder_wwf
 
+print('\nTOP SCORING HORIZONTAL AND VERTICAL PLAYS')
+print('FORMAT = (score, word, starting (i, j) position)')
+
 N = len(play_finder_wwf.BOARD)
 
 HOR_PLAYS = []
@@ -11,8 +14,8 @@ for i in range(N):
         HOR_PLAYS += play_finder_wwf.list_plays_and_scores((i, j))
 HOR_PLAYS.sort()
 
-print('HORIZONTAL PLAYS')
-for i in HOR_PLAYS:
+print('\nHORIZONTAL PLAYS')
+for i in HOR_PLAYS[-5:]:
     print(i)
 
 play_finder_wwf.flip_board()
@@ -22,7 +25,7 @@ for i in range(N):
         VER_PLAYS += play_finder_wwf.list_plays_and_scores((i, j))
 VER_PLAYS.sort()
 
-print('VERTICAL PLAYS')
-for i in VER_PLAYS:
+print('\nVERTICAL PLAYS')
+for i in VER_PLAYS[-5:]:
     i = (i[0], i[1], (i[2][1], i[2][0]))
     print(i)
